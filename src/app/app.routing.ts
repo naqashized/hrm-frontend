533ace78460beef896e7da22e740936e0a8e160a
tmcshared <tahirnaqashized@gmail.com>
2018-07-17 20:02:27 +0500

    unused items removed

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,70 +9,80 @@ export const AppRoutes: Routes = [{
   children: [{
     path: '',
     loadChildren: './dashboard/dashboard.module#DashboardModule'
-  }, {
-    path: 'email',
-    loadChildren: './email/email.module#EmailModule'
-  }, {
-    path: 'components',
-    loadChildren: './components/components.module#ComponentsModule'
-  }, {
-    path: 'icons',
-    loadChildren: './icons/icons.module#IconsModule'
-  }, {
-    path: 'cards',
-    loadChildren: './cards/cards.module#CardsModule'
-  }, {
-    path: 'forms',
-    loadChildren: './form/form.module#FormModule'
-  }, {
-    path: 'tables',
-    loadChildren: './tables/tables.module#TablesModule'
-  }, {
-    path: 'datatable',
-    loadChildren: './datatable/datatable.module#DatatableModule'
-  }, {
-    path: 'charts',
-    loadChildren: './charts/charts.module#ChartsModule'
-  }, {
-    path: 'maps',
-    loadChildren: './maps/maps.module#MapsModule'
-  }, {
-    path: 'pages',
-    loadChildren: './pages/pages.module#PagesModule'
-  }, {
-    path: 'taskboard',
-    loadChildren: './taskboard/taskboard.module#TaskboardModule'
-  }, {
-    path: 'calendar',
-    loadChildren: './fullcalendar/fullcalendar.module#FullcalendarModule'
-  }, {
-    path: 'media',
-    loadChildren: './media/media.module#MediaModule'
-  }, {
-    path: 'widgets',
-    loadChildren: './widgets/widgets.module#WidgetsModule'
-  }, {
-    path: 'social',
-    loadChildren: './social/social.module#SocialModule'
-  }, {
-    path: 'docs',
-    loadChildren: './docs/docs.module#DocsModule'
-  }]
-}, {
+  }, 
+//   {
+//     path: 'email',
+//     loadChildren: './email/email.module#EmailModule'
+//   }, {
+//     path: 'components',
+//     loadChildren: './components/components.module#ComponentsModule'
+//   }, {
+//     path: 'icons',
+//     loadChildren: './icons/icons.module#IconsModule'
+//   }, {
+//     path: 'cards',
+//     loadChildren: './cards/cards.module#CardsModule'
+//   }, {
+//     path: 'forms',
+//     loadChildren: './form/form.module#FormModule'
+//   }, {
+//     path: 'tables',
+//     loadChildren: './tables/tables.module#TablesModule'
+//   }, 
+  {
+    path: 'user',
+    loadChildren: './user/datatable.module#DatatableModule'
+  }
+ //{
+//     path: 'charts',
+//     loadChildren: './charts/charts.module#ChartsModule'
+//   }, {
+//     path: 'maps',
+//     loadChildren: './maps/maps.module#MapsModule'
+//   }, {
+//     path: 'pages',
+//     loadChildren: './pages/pages.module#PagesModule'
+//   }, {
+//     path: 'taskboard',
+//     loadChildren: './taskboard/taskboard.module#TaskboardModule'
+//   }, {
+//     path: 'calendar',
+//     loadChildren: './fullcalendar/fullcalendar.module#FullcalendarModule'
+//   }, {
+//     path: 'media',
+//     loadChildren: './media/media.module#MediaModule'
+//   }, {
+//     path: 'widgets',
+//     loadChildren: './widgets/widgets.module#WidgetsModule'
+//   }, {
+//     path: 'social',
+//     loadChildren: './social/social.module#SocialModule'
+//   }, {
+//     path: 'docs',
+//     loadChildren: './docs/docs.module#DocsModule'
+//   }]
+// }
+, 
+{
   path: '',
   component: AuthLayoutComponent,
   children: [{
     path: 'authentication',
     loadChildren: './authentication/authentication.module#AuthenticationModule'
-  }, {
-    path: 'error',
-    loadChildren: './error/error.module#ErrorModule'
-  }, {
-    path: 'landing',
-    loadChildren: './landing/landing.module#LandingModule'
-  }]
-}, {
-  path: '**',
-  redirectTo: 'error/404'
-}];
+  }
+  // , {
+  //   path: 'error',
+  //   loadChildren: './error/error.module#ErrorModule'
+  // }, {
+  //   path: 'landing',
+  //   loadChildren: './landing/landing.module#LandingModule'
+  // }
+  ]
+ }
+  ]
+// }, {
+//   path: '**',
+//   redirectTo: 'error/404'
+ }
+];
 
