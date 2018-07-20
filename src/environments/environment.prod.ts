73eb59a81af800bae94e24f8c641186c2772f8d7
tmcshared <tahirnaqashized@gmail.com>
2018-07-20 19:07:06 +0500

    environment url for production changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000/api/'
-  //baseUrl: 'http://ec2-18-221-39-77.us-east-2.compute.amazonaws.com:3000/api/'
+  //baseUrl: 'http://localhost:3000/api/'
+  baseUrl: 'http://ec2-18-221-39-77.us-east-2.compute.amazonaws.com:3000/api/'
 };
